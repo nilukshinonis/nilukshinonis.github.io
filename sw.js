a929b9990b006e76a2dbe70f6b0dd71e8753bc65
Nilukshi Nonis <nonisnilukshi@gmail.com>
2021-01-05 21:21:23 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4afb3a1079ac410a1a4d.js"
+    "url": "webpack-runtime-3c86d040c18f18d2e78c.js"
   },
   {
     "url": "framework-cd3e1e804d552fa282ef.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "34f459671cb6eb0dc9e6b2be418108e1"
+    "revision": "d34debeb26841b05a856af932e0ebb4a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "733dbbb6b8ec8933a880dc97a6cad75b"
+    "revision": "cbb1f1d1ffe9f8e972829ba2d83ce735"
   },
   {
     "url": "polyfill-91861f81ca7f0737d9ad.js"
