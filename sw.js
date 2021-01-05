188b88b37cac6431c0adee379b8f79360e51722a
Shenal Silva <shenal.silva007@gmail.com>
2021-01-05 20:11:42 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9c19cb9057f92e9477c9.js"
+    "url": "webpack-runtime-6ea44736ff56029f2a62.js"
   },
   {
     "url": "framework-cd3e1e804d552fa282ef.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6c51b92016b7b29124a7dc1c9d59a32f"
+    "revision": "7d9886f088162facde1e1a9cea6148d9"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
