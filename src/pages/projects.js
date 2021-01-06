a52c7f5ddda2e825287c8b18be2c8d18f993a9f1
Shenal Silva <shenal.silva007@gmail.com>
2021-01-06 17:26:32 +0100

    Rename

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,7 +8,6 @@ import project_halo_image from '../assets/images/project_halo.png';
 import project_plasticcycle_image from '../assets/images/project_plasticycle.png';
 import project_atlas_bottle_image from '../assets/images/project_atlas_bottle.png';
 
-
 const IndexPage = () => (
   <Layout fullMenu>
     <section id="wrapper">
@@ -21,44 +20,53 @@ const IndexPage = () => (
       <div className="wrapper">
         <div className="inner">
           <p>
-            My mantra in design is to always get the research on point.
-            I have switched between the roles of Industrial, Interior,
-            UX and UI designer and can seamlessly navigate between design tools, teams,
+            My mantra in design is to always get the research on point. I have
+            switched between the roles of Industrial, Interior, UX and UI
+            designer and can seamlessly navigate between design tools, teams,
             and disciplines.
           </p>
           <p>
-          Design to me is a never ending process that can be addictive.
-          Happiness to me comes when I see my designs being used by others with a smile on their face.
+            Design to me is a never ending process that can be addictive.
+            Happiness to me comes when I see my designs being used by others
+            with a smile on their face.
           </p>
           <p>
-            I take pride in all the projects 
-            I have done and consider them to be a mural of the power of collaboration between different disciplines coming together in design.
+            I take pride in all the projects I have done and consider them to be
+            a mural of the power of collaboration between different disciplines
+            coming together in design.
           </p>
           <section className="features">
             <article>
               <a href="/projects/govi-mithuru" className="image">
                 <img src={project_govi_mithuru_image} alt="Govi Mithuru App" />
               </a>
-              <h3 className="major">Govi Mithuru Service App</h3>
+              <a href="/projects/govi-mithuru">
+                <h3 className="major">Govi Mithuru Service App</h3>
+              </a>
               <p>
-                'Govi Mithuru' (ගොවි මිතුරු) service app, is a mobile advisory service for farmers and home growers in Sri Lanka.
-                The app provides customized and timely advice to farmers regarding land preparation, cultivation, crop protection, harvest
-                and improved family nutrition.
+                'Govi Mithuru' (ගොවි මිතුරු) service app, is a mobile advisory
+                service for farmers and home growers in Sri Lanka. The app
+                provides customized and timely advice to farmers regarding land
+                preparation, cultivation, crop protection, harvest and improved
+                family nutrition.
               </p>
               <a href="/projects/govi-mithuru" className="special">
                 See more about Govi Mithuru
               </a>
             </article>
             <article>
-              <a href="/#" className="image">
+              <a href="/viyana-quick-shade" className="image">
                 <img src={project_viyana_image} alt="Viyana Quick Shade" />
               </a>
-              <h3 className="major">Viyana Quick Shade</h3>
+              <a href="/viyana-quick-shade">
+                <h3 className="major">Viyana Quick Shade</h3>
+              </a>
               <p>
-                ‘Viyana’ (වියන) quick shade is a weather protection device for males in Sri Lanka that feels masculine,
-                is easy to use and provides quick cover and immediately dries after use.
+                ‘Viyana’ (වියන) quick shade is a weather protection device for
+                males in Sri Lanka that feels masculine, is easy to use and
+                provides quick cover and immediately dries after use.
               </p>
-              <a href="/#" className="special">
+              <a href="/viyana-quick-shade" className="special">
                 See more about Viyana Quickshade
               </a>
             </article>
@@ -68,8 +76,9 @@ const IndexPage = () => (
               </a>
               <h3 className="major">Cover Me App</h3>
               <p>
-                ‘CoverMe’ is a mobile app for a Sri Lankan office worker who forgets their umbrella.
-                The app enables them to rent an umbrella and never worry about bad weather again.
+                ‘CoverMe’ is a mobile app for a Sri Lankan office worker who
+                forgets their umbrella. The app enables them to rent an umbrella
+                and never worry about bad weather again.
               </p>
               <a href="/#" className="special">
                 See more about the Cover Me App
@@ -81,7 +90,8 @@ const IndexPage = () => (
               </a>
               <h3 className="major">Mommy Halo </h3>
               <p>
-                Mommy Halo is a  donut shaped iPad holder for pregnant mothers to safely and comfortably rest a tablet, on their pregnant belly.
+                Mommy Halo is a donut shaped iPad holder for pregnant mothers to
+                safely and comfortably rest a tablet, on their pregnant belly.
               </p>
               <a href="/#" className="special">
                 See more about Mommy Halo
@@ -93,8 +103,11 @@ const IndexPage = () => (
               </a>
               <h3 className="major">Plastic-cycle</h3>
               <p>
-                Plastic-cyle bin is a plastic collector that is placed all over Sri Lanka in the hope of collecting and recycling plastics.
-                The bin not only has been an icon and internal tool for the John Keells group plastic reduction and recycling initiative,but helps create new consumer behaviour.
+                Plastic-cyle bin is a plastic collector that is placed all over
+                Sri Lanka in the hope of collecting and recycling plastics. The
+                bin not only has been an icon and internal tool for the John
+                Keells group plastic reduction and recycling initiative,but
+                helps create new consumer behaviour.
               </p>
               <a href="/#" className="special">
                 See more about Plastic-cycle
@@ -102,11 +115,15 @@ const IndexPage = () => (
             </article>
             <article>
               <a href="/#" className="image">
-                <img src={project_atlas_bottle_image} alt="Atlas Water Bottles" />
+                <img
+                  src={project_atlas_bottle_image}
+                  alt="Atlas Water Bottles"
+                />
               </a>
               <h3 className="major">Atlas Water Bottles</h3>
               <p>
-                Atlas water bottles are the first mass scale locally designed and manufactured bottles in Srilanka that's fun and sporty,
+                Atlas water bottles are the first mass scale locally designed
+                and manufactured bottles in Srilanka that's fun and sporty,
                 encouraging kids to drink more water.
               </p>
               <a href="/#" className="special">
