b4eff8014ca1c1d1ca29682519802d0bf672cc9b
Nilukshi Nonis <nonisnilukshi@gmail.com>
2021-01-07 15:32:40 +0100

    Added research image to viyana

--- src/pages/projects/viyana-quick-shade.js
+++ src/pages/projects/viyana-quick-shade.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from '../../components/Layout';
-import magri_graph from '../../assets/images/project_image_govi_1.png';
+import viyana_research from '../../assets/images/project_image_viyana_research.png';
 import magri_frog_casestudyimage from '../../assets/images/project_image_govi_casestudy_img.png';
 import magri_frog_articleimage from '../../assets/images/project_image_govi_article_img.png';
 
@@ -18,24 +18,29 @@ const IndexPage = () => (
       <div className="wrapper">
         <div className="inner">
           <h3 className="major">The Project</h3>
+          <p>A weather protection device for males in Sri Lanka</p>
           <p>
-          A weather protection device for males in Sri Lanka
+            Rain and shine are two things Srilankans evolved with and respond
+            to, on a daily basis. Even though Sri Lanka experiences sun and
+            rain, the available weather protective products are limited. Locally
+            available products to get cover are umbrellas, rain jackets and
+            caps. Among these products umbrellas report 90% sales annually
+            (Rainco annual report, 2015).{' '}
           </p>
-          <h3 className="major">The Business Need</h3>
+          <p>
+            Rainco Pvt Ltd is a leading umbrella manufacturer in Sri Lanka. 75%
+            annual sales of the company come from ladies compactable umbrellas.
+            Only 15% sales come from the gents‘ non compactable umbrellas. The
+            company wanted to understand why males didn’t buy umbrellas and see
+            if a product can be designed according to their need.
+          </p>
+          
+          <h3 className="major">The Problem</h3>
           <h4>
-            How might we empower the livelihood of farmers through mobile
-            technology?
+          Why don’t males respond to weather protection devices?
           </h4>
-          <p>
-            Dialog a leading Mobile Network Operator (MNO) had been introducing
-            products services for farmers of Sri Lanka since 2011. Yet
-            statistics revealed only 21% farmers adopted to these services and
-            continued using them. So they teamed up with GSMA and Frog to test a
-            M-Agri toolkit with farmers of Sri Lanka in the hope of
-            understanding the real need of farmers.
-          </p>
-
-          <img className="image main" src={magri_graph} alt="" />
+          
+          <img className="image main" src={viyana_research} alt="" />
           <section className="features">
             <article>
               <a
