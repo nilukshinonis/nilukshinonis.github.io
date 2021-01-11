9255b45627600a63a9f0e51009e26b50960086c9
Nilukshi Nonis <nonisnilukshi@gmail.com>
2021-01-11 15:59:55 +0100

    finished adding all content to atlas project with final video

--- src/pages/projects/atlas-waterbottle.js
+++ src/pages/projects/atlas-waterbottle.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Layout from '../../components/Layout';
 import atlas_waterbottlesketch from '../../assets/images/project_image_atlas_waterbottle_sketch.png';
+import atlas_waterbottleprototype from '../../assets/images/project_image_atlas_waterbottle_prototypes.png';
 import magri_frog_casestudyimage from '../../assets/images/project_image_govi_casestudy_img.png';
 import magri_frog_articleimage from '../../assets/images/project_image_govi_article_img.png';
 
@@ -62,10 +63,47 @@ const IndexPage = () => (
             designing, detailing, Prototyping and making production drawings
             with simulation testing together with the engineers.
           </p>
-          <h3 className="major">The Problem</h3>
-          <h4>Why don’t males respond to weather protection devices?</h4>
 
           <img className="image main" src={atlas_waterbottlesketch} alt="" />
+          <h3 className="major">The Approach</h3>
+          <p>
+            The journey in design was always revolving around the end user. Our
+            walls were full of millions of sketches and ideas back in the
+            studio. Next it was modeling the sketches in blue foam and adding
+            the tactile quality to our design for our users to feel the design
+            and give their feedback. Atlas representatives took these prototypes
+            out of colombo and came back with a list that bugged the kids and
+            additions they prefer. Next it was modeling in 3D solidworks and
+            soon after that our favourite; 3d printing. Finally with the making
+            of the moulds alot of late nights with the engineering teams on
+            volume and blow moulding requirements helped us go into production.
+          </p>
+          <img className="image main" src={atlas_waterbottleprototype} alt="" />
+          <h3 className="major">Reflection</h3>
+          <p>
+            Since the product emerged to cater and fulfill the deep desire of
+            having a playful sporty water bottle as a companion kids kept on
+            purchasing the products and this resulted in the company rocketing
+            up sales from 200,000 per year to 1 million helping the brand
+            enlarge its product portfolio providing more options to the
+            customer.
+          </p>
+          <p>
+            Take a 10 minute walk in any part of Sri Lanka and you will see at
+            least one kid with an Atlas bottle. That is happiness to me. In our
+            initial research we found out that kids tend to dispose of their
+            water bottles annually at the beginning of each year. The addition
+            of emotional value to the product has made a clear change in user
+            behaviour and disposal of design.
+          </p>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/ez8wF9Ra8Ek"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
     </section>
