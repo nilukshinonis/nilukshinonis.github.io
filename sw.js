bb8217329ff7104bd69cb874f7fb6bf351f741ea
Nilukshi Nonis <nonisnilukshi@gmail.com>
2021-01-11 17:16:41 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,28 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4e9c71ff35a178c0b97a.js"
+    "url": "webpack-runtime-787049db099f474f759c.js"
   },
   {
     "url": "framework-c8d3a5345e3f321d7da5.js"
   },
   {
-    "url": "app-6093bdb883da4ebe8d99.js"
+    "url": "app-a97e879b76f999534dde.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7b95f3648363d02ee726e2fd12ca2b09"
+    "revision": "5cde18f07637f211a5b217ec22eea0bb"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
+  },
+  {
+    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
+    "revision": "f6081b83111aea4128c98944b7fafccc"
+  },
+  {
+    "url": "page-data/app-data.json",
+    "revision": "ba6f6cd5d437548ac3f35e796aa8a6d7"
   },
   {
     "url": "polyfill-7dd0b1ecbf932ac41ed8.js"
@@ -139,7 +147,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-6093bdb883da4ebe8d99.js`))) {
+  if (!resources || !(await caches.match(`/app-a97e879b76f999534dde.js`))) {
     return await fetch(event.request)
   }
 
