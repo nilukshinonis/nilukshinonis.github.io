4bd87e8be78140667c4196fd08aadb98d9b84177
shenal <shenal@users.noreply.github.com>
2023-01-15 19:32:01 +0000

    Deploying to gh-pages from @ nilukshinonis/nilukshinonis.github.io@ca9aa8ac33d46356da9a140e6c32126486aa376f 🚀

---
+++ component---src-pages-404-js-fa2834ac5bc519900c49.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknilukshinonis_com=self.webpackChunknilukshinonis_com||[]).push([[883],{429:function(e,n,t){t.r(n);var l=t(7294),a=t(3656);n.default=function(){return l.createElement(a.Z,{fullMenu:!0},l.createElement("section",{id:"wrapper"},l.createElement("header",null,l.createElement("div",{className:"inner"},l.createElement("h2",null,"Ooops"),l.createElement("p",null,"You landed on the wrong page."),l.createElement("a",{href:"/"},"Go back to the home page")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fa2834ac5bc519900c49.js.map
